b192d073fad17d123510a02dc6332779c4997335
Guilherme Estrela <guilhermestrela@gmail.com>
2025-02-25 18:01:02 -0300

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -12,25 +12,64 @@ document.addEventListener("DOMContentLoaded", () => {
     const buttonSend = document.querySelector(".cursor--pointer[src*='send']");
     console.log(buttonSend);
 
+    const listaMensagens = document.querySelector(".div--messages");
+    console.log(listaMensagens);
+
     //buttonSend.classList.add("minha-classe-modulo-um");
 
-    function sendAlert() {
+    const respostasParaOBot = [
+        "Olá, tudo bem?",
+        "Como você está?",
+        "O que você deseja?",
+        "Qual é o seu nome?",
+        "O meu nome é Bot",
+        "Eu faço parte do time de desenvolvimento",
+        "Você quer conversar sobre o que?",
+        "Qual é a sua idade?",
+        "O que você faz da vida?",
+    ];
+
+    function enviarMensagem() {
         const texto = inputMsg.value.trim();
         
         if (texto === "") {
             alert("Please enter a message");
         } else {
-            alert(`Your message is: ${texto}`);
+            adicionarMenssagem("enviada", texto);
+
+            setTimeout(responderMensagem, 2000);
         }
     }
 
+    function responderMensagem() {
+        const posicao = Math.floor(Math.random() * respostasParaOBot.length);
+        const mensagemDoBot = respostasParaOBot[posicao];
+        adicionarMenssagem("recebida", mensagemDoBot);
+    }
+
+    function adicionarMenssagem(tipoMensan, texto) {
+        const mensagemElement = document.createElement("div");
+
+        mensagemElement.classList.add("message");
+
+        if (tipoMensan === 'enviada') {
+            mensagemElement.classList.add("you");
+        } else {
+            mensagemElement.classList.add("other");
+        }
+
+        mensagemElement.innerText = texto;
+
+        listaMensagens.appendChild(mensagemElement);
+    } 
+
     buttonSend.addEventListener("click", () => {
-        sendAlert();
+        enviarMensagem();
     });
 
     inputMsg.addEventListener("keypress", (event) => {
         if (event.key === "Enter") {
-            sendAlert();
+            enviarMensagem();
         }
     });
 });
