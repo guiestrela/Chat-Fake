87963d7702fce45358bc5b7647b7a2de7d35daee
Guilherme Estrela <guilhermestrela@gmail.com>
2025-02-25 19:59:39 -0300

    update script and delete some HTML lines

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,34 @@
+const listaDeContatos = [
+    {
+        id: 1,
+        nome: "João",
+        ultimaMensagem: "Olá, tudo bem?",
+        horarioUltimaMensagem: "15:30",
+        avatar: "src/assets/images/Avatar00.png"
+    },
+    {
+        id: 2,
+        nome: "Mario",
+        ultimaMensagem: "Qual era o codigo?",
+        horarioUltimaMensagem: "15:30",
+        avatar: "src/assets/images/Avatar00.png"
+    },
+    {
+        id: 3,
+        nome: "Maria",
+        ultimaMensagem: "Olá, o projeto, está pronto?",
+        horarioUltimaMensagem: "15:30",
+        avatar: "src/assets/images/Avatar00.png"
+    },
+    {
+        id: 4,
+        nome: "Carla",
+        ultimaMensagem: "Tem café",
+        horarioUltimaMensagem: "15:30",
+        avatar: "src/assets/images/Avatar00.png"
+    }
+];
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("minha pagina carregou!");
 
@@ -36,6 +67,7 @@ document.addEventListener("DOMContentLoaded", () => {
             alert("Please enter a message");
         } else {
             adicionarMenssagem("enviada", texto);
+            inputMsg.value = "";
 
             setTimeout(responderMensagem, 2000);
         }
@@ -50,7 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function adicionarMenssagem(tipoMensan, texto) {
         const mensagemElement = document.createElement("div");
 
-        mensagemElement.classList.add("message");
+        mensagemElement.classList.add("message", "fade-in");
 
         if (tipoMensan === 'enviada') {
             mensagemElement.classList.add("you");
@@ -61,6 +93,10 @@ document.addEventListener("DOMContentLoaded", () => {
         mensagemElement.innerText = texto;
 
         listaMensagens.appendChild(mensagemElement);
+
+        setTimeout(() => {
+            mensagemElement.classList.remove("fade-in");
+        }, 500);
     } 
 
     buttonSend.addEventListener("click", () => {
@@ -72,4 +108,50 @@ document.addEventListener("DOMContentLoaded", () => {
             enviarMensagem();
         }
     });
+
+    function carregarContatos() {
+        const divContatosElement = document.querySelector(".div--contacts");
+
+        
+
+        listaDeContatos.forEach((contato) => {
+            console.log(contato);            
+            const divParentElement = document.createElement("div");
+            divParentElement.classList.add("flex", "area--contact", "fade-in");
+
+            divParentElement.innerHTML = `
+                    <div class="flex justify--content--center align--items--center flex--1">
+                        <img class="avatar--left--bar" src="${contato.avatar}" alt="">
+                    </div>
+
+                    <div class="flex flex--direction--column justify--content--center flex--3">
+                        <div>
+                            <div class="flex align--items--center infos--contact">    
+                            <div class="font--family font--weight--bold">${contato.nome}</div>
+                            <img src="src/assets/icons/verified.svg" alt="">
+                            </div>  
+                            
+                            <div class="last--message">${contato.ultimaMensagem}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex flex--direction--column justify--content--center align--items--end flex--1 div--last--messages--info">
+                        <div class="hour--last--message">
+                            ${contato.horarioUltimaMensagem}
+                        </div>
+                        <div class="flex justify--content--center align--items--center quantity--last--viewed--messages background--green">
+                            1
+                        </div>
+                    </div>
+                `;
+
+                
+                divContatosElement.appendChild(divParentElement);
+        });
+    }
+    setTimeout(() => {
+        carregarContatos();
+    }, 2500);
+    
 });
